56d8c814af8959a454c164ceaa92319289536bc5
Anna Kompan <annakompanch@gmail.com>
2025-07-16 23:41:27 +0300

    updated Time O(n + m)

--- count_negative_num_in_matrix.js
+++ count_negative_num_in_matrix.js
@@ -19,20 +19,38 @@ console.log(
   `There are ${countNegatives(grid2)} negative numbers in the matrix`
 );
 
+// function countNegatives(grid) {
+//   let negatives = 0;
+//   for (row of grid) {
+//     let left = 0;
+//     let right = row.length - 1;
+//     while (left <= right) {
+//       let mid = Math.floor(left + (right - left) / 2);
+//       if (row[mid] < 0) {
+//         right = mid - 1;
+//       } else {
+//         left = mid + 1;
+//       }
+//     }
+//     negatives += row.length - left;
+//   }
+//   return negatives;
+// }
+
 function countNegatives(grid) {
   let negatives = 0;
-  for (row of grid) {
-    let left = 0;
-    let right = row.length - 1;
-    while (left <= right) {
-      let mid = Math.floor(left + (right - left) / 2);
-      if (row[mid] < 0) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
+  let rows = grid.length;
+  let cols = grid[0].length;
+  let row = cols - 1;
+  let col = 0;
+
+  while (row >= 0 && col < cols) {
+    if (grid[row][col] < 0) {
+      negatives += rows - row;
+      row--;
+    } else {
+      col++;
     }
-    negatives += row.length - left;
   }
   return negatives;
 }
